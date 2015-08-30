c70edf2ac646eb5c7fea100f21930dab8682fbbf
Boris Town <tbziy@hotmail.com>
2015-08-30 13:53:38 +0800

    char TagList[MAX_TAG_COUNT][MAX_TAG_LENGTH];

--- Tagging/TagBase.h
+++ Tagging/TagBase.h
@@ -6,5 +6,11 @@
 //Define max tag input count in a line
 #define MAX_TAG_COUNT 128
 
+//Defin Taglist
+char TagList[MAX_TAG_COUNT][MAX_TAG_LENGTH];
+
+//Define Tag
+char Tag[MAX_TAG_LENGTH];
+
 //Tags
 typedef char TagName[MAX_TAG_LENGTH];
