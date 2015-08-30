a018b77afc9e174054849152c9a690c44126b10b
Boris Town <tbziy@hotmail.com>
2015-08-30 11:46:43 +0800

    #define MAX_TAG_COUNT 128

--- Tagging/Screen.h
+++ Tagging/Screen.h
@@ -2,6 +2,3 @@
 
 //Define max screen input length
 #define MAX_SCREENINPUT_LENGTH 1024
-
-//Define max tag input count in a line
-#define MAX_LINE_TAG_COUNT 128
--- Tagging/TagBase.h
+++ Tagging/TagBase.h
@@ -3,5 +3,8 @@
 //Define max tag name length
 #define MAX_TAG_LENGTH 128
 
+//Define max tag input count in a line
+#define MAX_TAG_COUNT 128
+
 //Tags
 typedef char TagName[MAX_TAG_LENGTH];
