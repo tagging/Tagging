75cb1b42fb0509d9ee400779142176826b482603
Boris Town <tbziy@hotmail.com>
2015-08-30 11:20:34 +0800

    Line* FileLines(char* fileName);

--- Tagging/FileBase.h
+++ Tagging/FileBase.h
@@ -1,5 +1,9 @@
 #pragma once
+//Get File Lines
+Line* FileLines(char* fileName);
 //Read file tags in Tags.txt
 FileTags* ReadTags();
 //Read sub-tags in Sub.txt
 FileSubs* ReadSubs();
+//Get file list in current folder
+FileName* GetFileList();
