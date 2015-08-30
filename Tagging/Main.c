3084b5392780097afe8894da413289a29744b6aa
Boris Town <tbziy@hotmail.com>
2015-08-30 15:10:58 +0800

    Screen.c Screen_Initial Screen_Tagging

--- Tagging/Main.c
+++ Tagging/Main.c
@@ -25,28 +25,6 @@ void FileScan()
 	SetDefaultTag(filetags, filenames);
 }
 
-//Process initial screen
-void Screen_Initial() {
-	//line string
-	char TagLine[MAX_SCREENINPUT_LENGTH];
-	//Print Welcome
-	printf("Please input tags separated by space:\n");
-	//Get input line
-	scanf_s("%[^\n]", &TagLine, MAX_SCREENINPUT_LENGTH);
-	//Read \n
-	getchar();
-	//Split input line into tags
-	strsplit(TagLine, &LineTagCount);
-}
-
-//Process tagging screen
-void Screen_Tagging() {
-	for (int i = 0; i < LineTagCount; i++)
-	{
-		printf("%s\n", TagList[i]);
-	}
-}
-
 int main()
 {
 	//Scan files for tags
