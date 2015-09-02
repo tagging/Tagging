22227b7a2d6b3e2802dd0c0c449fe9f575fb1372
Boris Town <tbziy@hotmail.com>
2015-09-02 22:24:43 +0800

    test driver module for FileBase_Lines(char* fileName)

--- TestProject/Main.c
+++ TestProject/Main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 
+//Define max input line length
+#define MAX_FILELINE_LENGTH 1024
+
+//Line
+typedef struct line
+{
+	//Line Count
+	int Count;
+	//Lines
+	char Lines[][MAX_FILELINE_LENGTH];
+}Line;
+
+//Get File Lines
+Line* FileBase_Lines(char* fileName)
+{
+	FILE* f = fopen("Test.txt", "rt");
+}
+
 //File Read Test
 int main()
 {
-	FILE* f = fopen("Test.txt", "rt");
+	//Read lines in file Tags.txt
+	Line* lines = FileLines("Tags.txt");
+	printf("Count = %i\n", lines->Count);
+	for (int i = 0; i < lines->Count; i++)
+	{
+		printf("%s\n", lines->Lines[i]);
+	}
 	return 0;
 }
