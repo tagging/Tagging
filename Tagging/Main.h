3348c671100ede6653cecd39b48f2d0c0b7fa606
Boris Town <tbziy@hotmail.com>
2015-09-19 15:31:00 +0800

    void Node_Sort(Node** nodes);

--- Tagging/Main.h
+++ Tagging/Main.h
@@ -1,10 +1,16 @@
 #pragma once
 
-#define _TEST_MODE_
 #define NODE_CONTENT_MAX_LENGTH 1024
 
 //Self pointer Node 
 typedef	struct node {
 	char content[NODE_CONTENT_MAX_LENGTH] ;
 	Node** links;
+	int linkCount;
 } Node;
+
+//Sort nodes
+void Node_Sort(Node** nodes);
+
+//Binary Search for nodes
+Node* Node_Search(Node** nodes, int nodeCount, char* saerchContent);
