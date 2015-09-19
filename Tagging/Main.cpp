d2def4216a9458a00dc131a0eba9edb493007b4b
Boris Town <tbziy@hotmail.com>
2015-09-19 19:46:33 +0800

    INIT DLL

--- Tagging/Main.cpp
+++ Tagging/Main.cpp
@@ -12,14 +12,22 @@ _declspec(dllexport) Node* Node_Search(
 	int nodeCount, 
 	char* saerchContent
 	) {
-	Node* node;
+	Node* node = 0;
 	return node;
 }
 
-BOOL APIENTRY DllMain(
-	HANDLE hModule,
+BOOL APIENTRY DllMain(HMODULE hModule,
 	DWORD  ul_reason_for_call,
 	LPVOID lpReserved
-	) {
+	)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
 	return TRUE;
 }
--- Tagging/Main.h
+++ Tagging/Main.h
@@ -6,8 +6,8 @@
 
 //Self pointer Node 
 typedef	struct node {
-	char content[NODE_CONTENT_MAX_LENGTH] ;
-	Node** links;
+	char content[NODE_CONTENT_MAX_LENGTH];
+	struct node** links;
 	int linkCount;
 } Node;
 
@@ -15,4 +15,8 @@ typedef	struct node {
 _declspec(dllexport) void Node_Sort(Node** nodes);
 
 //Binary Search for nodes
-_declspec(dllexport) Node* Node_Search(Node** nodes, int nodeCount, char* saerchContent);
+_declspec(dllexport) Node* Node_Search(
+	Node** nodes,
+	int nodeCount,
+	char* saerchContent
+	);
